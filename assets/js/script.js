6775acfa6922bfc1a147d0fcbbbb2209a168edab
User <iqbalmahad4039@gmail.com>
2025-04-25 10:21:34 +0700

    update nav dan isinya

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,26 @@ const iconToggle = document.querySelector(".icon-toggle input");
 const nav = document.querySelector(".nav-device");
 const menu = document.querySelector(".menu");
 const sub = document.querySelectorAll(".sub");
+const acc = document.querySelectorAll(".accordion");
 
-iconToggle.addEventListener("click",function(){
+acc.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    panel.style.display = panel.style.display === "block" ? "none" : "block";
+  });
+});
+
+iconToggle.addEventListener("click", function () {
   nav.classList.toggle("show");
 });
 
-sub.forEach(function(subButton){
-  subButton.addEventListener("click",function(){
+sub.forEach(function (subButton) {
+  subButton.addEventListener("click", function () {
     console.log("Bismillah Submenu");
     const subMenu = this.nextElementSibling;
-    if(subMenu){
+    if (subMenu) {
       subMenu.classList.toggle("show");
     }
-
   });
 });
